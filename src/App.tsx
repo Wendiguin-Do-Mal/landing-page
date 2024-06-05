1a067477750503558c33ec8737ca188003013f5d
Wendiguin-Do-Mal <sniffyape@gmail.com>
2024-06-04 21:53:28 -0300

    feat.add Form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,20 @@
-import { useState } from 'react';
 import './App.css'
+import Form from './components/Form'
 
 function App() {
-  // let name: string;
-  let [name, setName] = useState<string>()
-  let [email, setEmail] = useState<string>()
-  let [cpf, setCpf] = useState<string>()
-  let [tel, setTel] = useState<string>()
-  let [adress, setAdress] = useState<string>()
-  let [country, setCountry] = useState<string>()
+ 
 
   return( 
   <>
-    <section>
-      <form action="submit">
-        <fieldset>
-          <label htmlFor="name">Nome:</label>
-          <input name='name' type="text" onChange={(e)=>setName(e.target.value)} value={name} />
-        </fieldset>
-
-        <fieldset>
-          <label htmlFor="email">Email:</label>
-          <input name='email' type="text" onChange={(e)=>setEmail(e.target.value)} value={email} />
-        </fieldset>
-
-        <fieldset>
-          <label htmlFor="cpf">CPF:</label>
-          <input name='cpf' type="text" onChange={(e)=>setCpf(e.target.value)} value={cpf}/>
-        </fieldset>
-
-        <fieldset>
-          <label htmlFor="tel">Telefone:</label>
-          <input name='tel' type="text" onChange={(e)=>setTel(e.target.value)} value={tel} />
-        </fieldset>
-
-        <fieldset>
-          <label htmlFor="adress">Endereço:</label>
-          <input name='adress' type="text"  onChange={(e)=>setAdress(e.target.value)} value={adress}/>
-        </fieldset>
-
-        <fieldset>
-          <label htmlFor="country">Cidade:</label>
-          <input name='country' type="text" onChange={(e)=>setCountry(e.target.value)} value={country} />
-        </fieldset>
-         
-         <button type='submit'>Enviar</button>
-         
-      </form>
-    </section>
-
+   <div style={{
+          display:"flex",
+          flexDirection:"column",
+          justifyContent:"center",
+          alignItems:"center",
+          width:"100vw",
+              }}>
+   <Form/>
+   </div>
   </>)
 }
 
